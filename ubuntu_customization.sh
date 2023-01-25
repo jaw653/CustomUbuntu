e3f0e79ebff43bf908369b0f6e58565e8def4c7f
Jake Wachs <jawachs@crimson.ua.edu>
2023-01-25 13:13:57 -0500

    updated output of script

--- ubuntu_customization.sh
+++ ubuntu_customization.sh
@@ -5,58 +5,59 @@
 #################################
 echo [*] Installing packages...
 
+touch setup_log.txt
 
 # Update the package list
-sudo apt-get update
+sudo apt-get update >> setup_log.txt
 
 # Install Vim
-sudo apt-get install -y vim
+sudo apt-get install -y vim >> setup_log.txt
 
 # Install tmux
-sudo apt-get install -y tmux
+sudo apt-get install -y tmux >> setup_log.txt
 
 # Install Hydra
-sudo apt-get install -y hydra
+sudo apt-get install -y hydra >> setup_log.txt
 
 # Install Hashcat
-sudo apt install hashcat
+sudo apt install hashcat >> setup_log.txt
 
 # Install Gobuster
-sudo apt-get install -y gobuster
+sudo apt-get install -y gobuster >> setup_log.txt
 
 # Install Nmap
-sudo apt-get install -y nmap
+sudo apt-get install -y nmap >> setup_log.txt
 
 # Install Sublime Text
-sudo apt-get install -y sublime-text
+sudo apt-get install -y sublime-text >> setup_log.txt
 
 # Install Wireshark
-sudo apt-get install -y wireshark
+sudo apt-get install -y wireshark >> setup_log.txt
 
 # Install Aircrack-ng
-sudo apt-get install -y aircrack-ng
+sudo apt-get install -y aircrack-ng >> setup_log.txt
 
 # Install John the Ripper
-sudo apt-get install -y john
+sudo apt-get install -y john >> setup_log.txt
 
 # Install Sqlmap
-sudo apt-get install -y sqlmap
+sudo apt-get install -y sqlmap >> setup_log.txt
 
 # Install Visual Studio Code
-sudo snap install code --classic
+sudo snap install code --classic >> setup_log.txt
 
 # Install Docker
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-sudo usermod -aG docker $USER
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common >> setup_log.txt
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - >> setup_log.txt
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" >> setup_log.txt
+sudo apt-get update >> setup_log.txt
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io >> setup_log.txt
+sudo usermod -aG docker $USER >> setup_log.txt
 
 # Install Metasploit
-curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
-chmod 755 msfinstall
-./msfinstall
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall >> setup_log.txt
+chmod 755 msfinstall >> setup_log.txt
+./msfinstall >> setup_log.txt
 
 ######################
 ##### Burp Suite #####
